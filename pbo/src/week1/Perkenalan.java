68c5a4852e5631f873c261967686576805a4c326
IlhamAhmadi <128119869+IlhamAhmadi@users.noreply.github.com>
2024-03-15 11:08:20 +0700

    Add files via upload

---
+++ pbo/src/week1/Perkenalan.java
@@ -0,0 +1,18 @@
+package week1;
+
+public class Perkenalan {
+
+	public static void main(String[] args) {
+		// Perkenalan
+		
+		String nama = "Ilham Ahmadi";
+		String college = "Universitas Dian Nuswantoro";
+		String prodi = "Teknik Informatika";
+		
+		System.out.println("Halo nama saya adalah " + nama);
+		System.out.println("Saya adalah mahasiswa " + prodi + " di " + college);
+		System.out.println("Saya sedang menulis program java");
+
+	}
+
+}
